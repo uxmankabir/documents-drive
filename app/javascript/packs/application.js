506345e3c7ca0d80debbad70558552a58edb05dc
Usman Kabir <usman.kabir.k@gmail.com>
2021-03-14 00:23:31 +0500

    bootstrap added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,19 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import * as bootstrap from 'bootstrap'
+import "../stylesheets/application"
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+  })
+
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+
+});
